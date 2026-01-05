50e49191fa2b6f54b96ccc3755287739ee106b73
SnoopyJackson <jonathangabaret@gmail.com>
2026-01-05 15:56:26 +0100

    add butterfly

--- script.js
+++ script.js
@@ -179,7 +179,15 @@ function selectGuard(guardId) {
             <img src="${guard.image}" alt="${guard.name}" class="guard-image">
             
             <div class="detail-section">
-                <h3>📋 DESCRIPTION</h3>
+                <div class="description-header">
+                    <h3>📋 DESCRIPTION</h3>
+                    <button class="watch-video-btn" onclick="${guard.video ? `window.open('${guard.video}', '_blank')` : `searchYouTube('${guard.name}', 'BJJ guard')` }">
+                        <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+                            <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
+                        </svg>
+                        <span>Watch Tutorial</span>
+                    </button>
+                </div>
                 <p>${guard.description}</p>
             </div>
             
